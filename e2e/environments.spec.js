4ce5b5f5a5f6ef87258d35235f838fa843f1dd56
Alexey Prokhorov <megahertz@l96.ru>
2025-10-28 11:21:57 +0700

    feat(e2e): Add one more SECRYPT_KEYS test

--- e2e/environments.spec.js
+++ e2e/environments.spec.js
@@ -7,6 +7,25 @@ const useDir = require('./helpers/useDir');
 describe('Environments', () => {
   const advancedPath = useDir(path.join(__dirname, 'fixtures/advanced'));
 
+  it('should load single key from SECRYPT_KEYS', async () => {
+    await runSecrypt(['encrypt'], { cwd: advancedPath.path });
+
+    await advancedPath.rmProjectDecryptedFiles();
+    await advancedPath.rm('secrypt.keys');
+    await runSecrypt(['decrypt'], {
+      cwd: advancedPath.path,
+      env: { SECRYPT_KEYS: 'dev: devtest' },
+    });
+
+    expect(await advancedPath.readDir()).toEqual([
+      '.env.dev',
+      '.env.dev.enc',
+      '.env.prod.enc',
+      'secrypt.config.js',
+      'src',
+    ]);
+  });
+
   it('should load keys from SECRYPT_KEY_{ENV}', async () => {
     await runSecrypt(['encrypt'], { cwd: advancedPath.path });
 
